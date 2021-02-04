3d905844e75ef98bd8de4126a4a6c026c9f02949
Chicabytes <chocobarjulietain@gmail.com>
2021-02-04 17:38:55 -0300

    Started the proyect and finished the timer

---
+++ OnTime/OnTime/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace OnTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.InfoLabel = new System.Windows.Forms.Label();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InfoLabel
+            // 
+            this.InfoLabel.AutoSize = true;
+            this.InfoLabel.Font = new System.Drawing.Font("Segoe UI", 65F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.InfoLabel.Location = new System.Drawing.Point(48, 20);
+            this.InfoLabel.Name = "InfoLabel";
+            this.InfoLabel.Size = new System.Drawing.Size(0, 116);
+            this.InfoLabel.TabIndex = 0;
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TimeLabel.Location = new System.Drawing.Point(12, 9);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(92, 19);
+            this.TimeLabel.TabIndex = 1;
+            this.TimeLabel.Text = "Current Time:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 161);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.InfoLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "On Time Clock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label InfoLabel;
+        private System.Windows.Forms.Label TimeLabel;
+    }
+}
+
---
+++ OnTime/OnTime/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StartTime();
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void StartTime()
+        {
+            Timer timer = new Timer();
+            timer.Enabled = true;
+            timer.Interval = 1000; // 1 seconds
+            DateTime SystemStartTime = DateTime.Now.AddMilliseconds(-Environment.TickCount);
+            var Uptime = DateTime.Now - SystemStartTime;
+            timer.Tick += (o, args) =>
+            {
+                InfoLabel.Text = (DateTime.Now - SystemStartTime).ToString(@"hh\:mm\:ss");
+            };
+            timer.Start();
+        }
+    }
+}
